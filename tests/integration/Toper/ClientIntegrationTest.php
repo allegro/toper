248ee4108b79ed7e84ea0b3eb5199e5e30e3259c
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2014-03-11 19:03:01 +0100

    integration test for post request

--- tests/integration/Toper/ClientIntegrationTest.php
+++ tests/integration/Toper/ClientIntegrationTest.php
@@ -10,6 +10,8 @@ class ClientIntegrationTest extends \PHPUnit_Framework_TestCase
 
     const HOST3 = "http://localhost:7800";
 
+    const HOST4 = "http://localhost:7900";
+
     /**
      * @test
      */
@@ -50,6 +52,23 @@ class ClientIntegrationTest extends \PHPUnit_Framework_TestCase
         $client = new Client($hostPoolProvider, new GuzzleClientFactory());
 
         $request = $client->get("/request");
+
+        $response = $request->send();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('ok', $response->getBody());
+    }
+
+
+    /**
+     * @test
+     */
+    public function shouldSendPostRequest()
+    {
+        $hostPoolProvider = new StaticHostPoolProvider(array(self::HOST4, self::HOST1));
+        $client = new Client($hostPoolProvider, new GuzzleClientFactory());
+
+        $request = $client->post("/should_be_post");
+        $request->setBody("data");
 
         $response = $request->send();
         $this->assertEquals(200, $response->getStatusCode());
--- tests/server/server.js
+++ tests/server/server.js
@@ -1,5 +1,28 @@
 var http = require('http');
 
+console.log("http://localhost:7900/should_be_post");
+
+http.createServer(function (req, res) {
+    console.log(req.url);
+    if (req.url == '/should_be_post') {
+        var data = "";
+        req.on("data", function (chunk) {
+            data += chunk;
+        });
+
+        req.on("end", function () {
+            if (req.method == 'POST' && data == "data") {
+                res.writeHead(200);
+                res.end("ok");
+            } else {
+                res.writeHead(400);
+                res.end("bad request");
+            }
+        });
+
+    }
+}).listen(7900);
+
 codes = {
     0: {port: 7800, message: ""},
     200: {port: 7820, message: "ok"},
@@ -9,10 +32,10 @@ codes = {
 
 for (var code in codes) {
     console.log("start server on port: " + codes[code].port);
-    createServer(code);
+    createPortServer(code);
 }
 
-function createServer(code) {
+function createPortServer(code) {
     http.createServer(function (req, res) {
         res.writeHead(code);
         res.end(codes[code].message)
