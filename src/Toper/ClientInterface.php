f4c8c46b29344ee79cb7966b191f2e3124cf644e
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2014-03-15 14:18:08 +0100

    add client interface methods

--- src/Toper/ClientInterface.php
+++ src/Toper/ClientInterface.php
@@ -5,16 +5,26 @@ interface ClientInterface
 {
     /**
      * @param string $url
+     * @param array $binds
      *
      * @return Request
      */
-    public function get($url);
+    public function get($url, array $binds = array());
 
 
     /**
      * @param string $url
+     * @param array $binds
      *
      * @return Request
      */
-    public function post($url);
+    public function post($url, array $binds = array());
+
+    /**
+     * @param string $url
+     * @param array $binds
+     *
+     * @return Request
+     */
+    public function put($url, array $binds = array());
 }
