e70229e2141e3d969d36f0b8c8e850cf96c03d6e
Grzegorz Wieczorek <grzegorz.wieczorek@allegro.pl>
2014-06-13 10:26:52 +0200

    Simple shuffle strategy for hosts pool

--- src/Toper/StaticHostPoolProvider.php
+++ src/Toper/StaticHostPoolProvider.php
@@ -14,6 +14,7 @@ class StaticHostPoolProvider implements HostPoolProviderInterface
      */
     public function __construct(array $hosts)
     {
+        shuffle($hosts);
         $this->hosts = $hosts;
     }
 
